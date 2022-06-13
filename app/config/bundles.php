a1710700bb77ca32a79b3d142d1cae82bc6198d4
Tristan Schildknecht <tschildknecht@debutant-e.pro>
2022-06-13 14:19:34 +0200

    :bug: fix database

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
