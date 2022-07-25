4baaba908ae1521c1667673ff971931a0ec5d3ab
Tristan Schildknecht <tschildknecht@debutant-e.pro>
2022-07-25 16:53:03 +0200

    :recycle: fix side-bar

--- app/src/Menu/MenuBuilder.php
+++ app/src/Menu/MenuBuilder.php
@@ -14,8 +14,7 @@ class MenuBuilder
 
     const CSS_CLASSES = [
         'ROOT' => 'nav-item',
-        'MENU' => 'nav-link collapsed m-0',
-        'SUBMENU' => 'nav-link-text ms-1 sidemenu-submenu collapse ps-2',
+        'MENU' => 'nav-link-text ms-1 ps-2',
         'ITEM' => 'sidenav-normal sidemenu-item',
         'LINK' => 'nav-link m-0'
     ];
@@ -26,7 +25,40 @@ class MenuBuilder
     public function __construct(
         private readonly FactoryInterface       $factory,
         private readonly ParameterBagInterface  $parameterBag
-    ){
+    )
+    {
+        $this->menu = $this->factory->createItem('root', [
+            'navbar' => true,
+            'childrenAttributes' => [
+                'id' => 'side-menu',
+                'class' => self::CSS_CLASSES['ROOT']
+            ]
+        ]);
 
+    }
+
+    final public function createAsideMenu(RequestStack $requestStack, TokenStorageInterface $storage): ItemInterface
+    {
+        $this->setEntriesForAdmin();
+        return $this->menu;
+    }
+
+    private function setEntriesForAdmin(): void
+    {
+        $this->menu->addChild('Dashboard', [
+            'route' => 'app_home',
+            'attributes' => ['class' => self::CSS_CLASSES['ITEM']],
+            'linkAttributes' => ['class' => self::CSS_CLASSES['MENU']],
+            'label' => '<span class="sidenav-normal">Dashboard</span>',
+            'extras' => array('safe_label' => true)
+        ]);
+
+        $this->menu->addChild('Users', [
+            'route' => 'app_home',
+            'attributes' => ['class' => self::CSS_CLASSES['ITEM']],
+            'linkAttributes' => ['class' => self::CSS_CLASSES['MENU']],
+            'label' => '<span class="sidenav-normal">J\'ajoute le reste demain</span>',
+            'extras' => array('safe_label' => true)
+        ]);
     }
 }
