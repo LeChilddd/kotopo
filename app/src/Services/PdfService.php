d6810b56ac6ec2d53630bc796478caf7dd84ec67
Deb <deborahdev.io@gmail.com>
2022-07-26 15:23:49 +0200

    :construction: pdf menu

---
+++ app/src/Services/PdfService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class PdfService
+{
+    private Dompdf $domPdf;
+
+    public function __construct(){
+        $this->domPdf = new DomPdf();
+
+        $pdfOptions = new Options();
+
+        $pdfOptions->set('defaultFont', 'Garamond');
+
+        $this->domPdf->setOptions($pdfOptions);
+    }
+
+    public function  showPdfFile($html): void
+    {
+         $this->domPdf->loadHtml($html);
+         $this->domPdf->render();
+         $this->domPdf->stream("Carte-Boisson-Kotopo.pdf", [
+             'Attachement' => false
+         ]);
+    }
+}
