0bc85423082054d6cb04f8028b9f0678d6b3ae0e
Deb <deborahdev.io@gmail.com>
2022-07-26 15:23:49 +0200

    :construction: pdf menu

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\PdfService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,4 +33,11 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/pdf', name: 'menu.pdf')]
+    public function test(PdfService $pdf): void {
+        $html = 'test';
+        $pdf->showPdfFile($html);
+    }
+
 }
