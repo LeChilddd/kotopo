9800186b2810dc0ea6c89f81e7413e46a1ea1fe2
Deb <deborahdev.io@gmail.com>
2022-07-28 18:40:57 +0200

    :construction: functionnal contact form

--- app/src/Controller/ContactController.php
+++ app/src/Controller/ContactController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(
+        Request $request,
+        EntityManagerInterface $manager
+    ): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $contact = $form->getData();
+
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre message à bien été envoyé',
+            );
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('pages/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
