4b8b25cbbe274104c545b722ff20b222634e292b
Deb <deborahdev.io@gmail.com>
2022-08-21 17:23:17 +0200

    :construction: modify profile

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -2,17 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    /**
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route('/user_profil/{id}', name: 'update_profil', methods: ['GET', 'POST'])]
+    public function updateProfil(
+        User $user,
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordHasherInterface $hasher,
+    ): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        if(!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if($this->getUser() !== $user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+
+                $user = $form->getData();
+                $manager->persist($user);
+                $manager->flush($user);
+
+                $this->addFlash(
+                    'success',
+                    'Vos modifications ont bien été prises enregistrées'
+                );
+                return $this->redirectToRoute('app_home');
+
+
+        }
+
+        return $this->render('pages/user_profil.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- app/src/Form/UserType.php
+++ app/src/Form/UserType.php
@@ -4,25 +4,115 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('cardNumber')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('gender')
-            ->add('createdAt')
-            ->add('lastLogin')
-            ->add('expiredAt')
-            ->add('membership')
+
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 100
+                ],
+                'label' => 'Nom / Prénom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 100]),
+                    new Assert\NotBlank(),
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 100
+                ],
+                'label' => 'Nom / Prénom',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 100]),
+                    new Assert\NotBlank(),
+                ]
+            ])
+            ->add('gender',TextType::class, [
+        'attr' => [
+            'class' => 'form-control',
+            'minlength' => 1,
+            'maxlength' => 25
+        ],
+        'label' => 'Genre',
+        'label_attr' => [
+            'class' => 'form-label mt-4'
+        ],
+        'constraints' => [
+            new Assert\Length(['min' => 2, 'max' => 100]),
+            new Assert\NotBlank(),
+        ]
+    ])
+
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => 2,
+                    'maxlength' => 180
+                ],
+                'label' => 'Adresse Email',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 180]),
+                    new Assert\NotBlank(),
+                    new Assert\Email(),
+                ]
+            ])
+
+
+
+
+            ->add('cardNumber',NumberType::class, [
+        'attr' => [
+            'class' => 'form-control',
+            'minlength' => 8,
+            'maxlength' => 8
+        ],
+        'label' => 'Numéro de carte',
+        'label_attr' => [
+            'class' => 'form-label mt-4'
+        ],
+        'constraints' => [
+            new Assert\Length(['min' => 8, 'max' => 8]),
+            new Assert\NotBlank(),
+        ]
+    ])
+            ->add('submit', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' => 'Modifier mon profil'
+            ])
+
+    /*        ->add('createdAt')*/
+           /* ->add('lastLogin')*/
+           /* ->add('expiredAt')*/
+         /*   ->add('membership')*/
         ;
     }
 
