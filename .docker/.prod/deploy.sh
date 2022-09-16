6efa32b908a411cb886265ad869c9878378e5271
Tristan Schildknecht <tschildknecht@debutant-e.pro>
2022-09-16 14:43:25 +0200

    :sparkles: last commit

--- .docker/.prod/deploy.sh
+++ .docker/.prod/deploy.sh
@@ -5,8 +5,8 @@ set -e
 docker-compose pull
 
 #Set maintenance mode to perform critical operations (database, upgrades, ...)
-APP_MAINTENANCE=1 docker-compose up -d
-#i.e: docker-compose exec -T app bin/console doctrine:migration:migrate -n
+APP_MAINTENANCE=1 docker-compose up -d -f ./docker-compose.yml
+docker-compose exec -T app bin/console doctrine:migration:migrate -n
 sleep 10; #for demo purpose
 
 #back to prod
